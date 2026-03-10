c9eeb95d0394cb7e6f5a9318e816358adf333b5a
riccardomontedori <montedori.riccardo@franchettisalviani.net>
2026-03-10 12:19:24 +0100

    Inserimento Classi

---
+++ progettoPokemon/src/progettopokemon/Rwolet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progettopokemon;
+
+/**
+ *
+ * @author montedori.riccardo
+ */
+public class Rwolet {
+    
+}
