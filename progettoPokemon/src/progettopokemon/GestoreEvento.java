8416306cb7889f12ab1d45a095960db915ccc3bb
riccardomontedori <montedori.riccardo@franchettisalviani.net>
2026-03-11 16:02:33 +0100

    Modifica ApplicaEvento
    Modifica fatta per non far capitare solo la bacca come oggetto trovabile

--- progettoPokemon/src/progettopokemon/GestoreEvento.java
+++ progettoPokemon/src/progettopokemon/GestoreEvento.java
@@ -12,7 +12,7 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class GestoreEvento {
-    
+
     private ArrayList<Evento> eventi;
     private Random r;
 
@@ -28,21 +28,31 @@ public class GestoreEvento {
         int indice = r.nextInt(eventi.size());
         return eventi.get(indice);
     }
-    
+
     public void ApplicaEvento(Evento e, Pokemon p, Inventario i) {
+        Random randomOggetto = new Random();
         switch (e) {
             case TROVA_OGGETTO:
-                i.setN_bacche(i.getN_bacche() + 1);
+                int scelta = randomOggetto.nextInt(4);
+
+                if (scelta == 0) {
+                    i.setN_acqua(i.getN_acqua() + 1);
+                } else if (scelta == 1) {
+                    i.setN_bacche(i.getN_bacche() + 1);
+                } else if (scelta == 2) {
+                    i.setN_pozioni(i.getN_pozioni() + 1);
+                } else {
+                    i.setN_revitalizzanti(i.getN_revitalizzanti() + 1);
+                }
                 break;
-                
             case TEAM_ROCKET:
                 p.setVita(p.getVita() - 15);
                 break;
-                
+
             case POKEMON_SELVATICO:
                 p.setVita(p.getVita() - 5);
                 break;
         }
-        p.PokemonMorto(); 
+        p.PokemonMorto();
     }
 }
